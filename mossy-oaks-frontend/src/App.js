949b9575a016e8d6b67f99610f1d993f4df5b68d
Erick-EEV <EEVERICKEEV@gmail.com>
2021-05-21 15:06:20 -0400

    Login Linked

--- mossy-oaks-frontend/src/App.js
+++ mossy-oaks-frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About'
 import Home from './components/Home'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
     <Home/>
     <About/>
+    <Login/>
     </div>
   );
 }
