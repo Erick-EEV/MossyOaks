0f2bde55e4df28b8fcabea6034af3416ac87770e
Erick-EEV <EEVERICKEEV@gmail.com>
2021-05-27 12:50:51 -0400

    updated

--- mossy-oaks-frontend/src/App.js
+++ mossy-oaks-frontend/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import About from './components/About'
 import Home from './components/Home'
-import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
     <Home/>
-    <About/>
-    <Login/>
     </div>
   );
 }
