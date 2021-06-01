6d0a36758b7aa96d67a6d213bdf8fb5626a4aea8
Erick-EEV <EEVERICKEEV@gmail.com>
2021-05-31 21:59:19 -0400

    Mossy Oaks title

--- mossy-oaks-frontend/src/components/About.jsx
+++ mossy-oaks-frontend/src/components/About.jsx
@@ -2,12 +2,12 @@ import {React, useState} from 'react';
 
 const About = () => {
 
-    
+
     const [hello,useHello] = useState("Hello World");
     return (
         <div>
             <h2>About</h2>
-
+            <h1>Mossy Oaks</h1>
         </div>
     );
 }
