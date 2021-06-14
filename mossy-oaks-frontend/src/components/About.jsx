4b1b7393ac9a3570c3060bdf1a7943719878d123
Erick-EEV <EEVERICKEEV@gmail.com>
2021-06-14 13:42:58 -0400

    cleaned About

--- mossy-oaks-frontend/src/components/About.jsx
+++ mossy-oaks-frontend/src/components/About.jsx
@@ -1,19 +1,13 @@
-import {React, useState} from 'react';
+import { React, useState } from "react";
 
 const About = () => {
-
-
- 
-    return (
-        <div>
-            <h2>About</h2>
-            <h1>Mossy Oaks</h1>
-            <p>Our mission is to bring you the lawn of your dreams</p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h2>About</h2>
+      <h1>Mossy Oaks</h1>
+      <p>Our mission is to bring you the lawn of your dreams</p>
+    </div>
+  );
+};
 
 export default About;
-
-
-
