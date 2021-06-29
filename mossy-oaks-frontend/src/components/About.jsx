341283fd9db7e714069b2c3db0bab2ecb14720df
Erick-EEV <EEVERICKEEV@gmail.com>
2021-06-28 21:26:00 -0400

    intial hieght

--- mossy-oaks-frontend/src/components/About.jsx
+++ mossy-oaks-frontend/src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <h2>About</h2>
       <h1>Mossy Oaks</h1>
       <p>Our mission is to bring you the lawn of your dreams</p>
-      <img alt="Home Lawn Design" src="https://www.mymove.com/wp-content/uploads/2021/01/Landscaping-ideas_Breadmaker_Shutterstock.jpg" width="500"
+      <img alt="Home Lawn Design" src="https://www.mymove.com/wp-content/uploads/2021/01/Landscaping-ideas_Breadmaker_Shutterstock.jpg" width="500" height="600"
 
       />
     </div>
